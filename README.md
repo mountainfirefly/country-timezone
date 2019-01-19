a71630a7cebc9b56ebc120807691c6d93f22935b
mountainfirefly <pankajdcoder@gmail.com>
2019-01-19 12:39:57 +0530

    add handling to find timezone with city name

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # country-timezone
-Find timezone with country code and country name
+Find timezone with city name, country name and country code
 
 #### Install
 ```
@@ -10,13 +10,14 @@ OR install with yarn
 yarn add country-timezone
 ```
 
-#### Get time with country name and country code
+#### Get time with city name, country name and country code
 ```
 import countryTimezone from 'country-timezone';
 
-const timezonesWithName = countryTimezone.getTimezonesWithCountryName('India');
+const timezonesWithCountryName = countryTimezone.getTimezones('India');
+const timezonesWithCityName = countryTimezone.getTimezones('New Delhi');
 const timezonesWithCode = countryTimezone.getTimezonesWithCountryCode('IN');
 
-console.log(timezonesWithName, timezonesWithCode);
-// [ 'Asia/Kolkata' ] [ 'Asia/Kolkata' ]
+console.log(timezonesWithCountryName, timezonesWithCityName, timezonesWithCode, );
+// [ 'Asia/Kolkata' ] [ 'Asia/Kolkata' ] [ 'Asia/Kolkata' ]
 ```
--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 var rawData = require('./data.json');
 
-function getTimezonesWithCountryName(name) {
+// Find timezones with city name and country name.
+function getTimezones(name) {
   if (!name) {
     return []
   }
 
   const countries = Object.values(rawData)
   const matchedCountry = countries.find(function(country) {
+    // If country doesn't have cities array in it.
+    if (!country.cities) {
+      country.cities = []
+    }
 
     const removedSpaceName = name.replace(/^\s+|\s+$/g, '');
     const lowerCountryName = country.name.toLowerCase().trim()
     const userEnteredCountryName = removedSpaceName.toLowerCase()
 
-    // If user has entered both country name and city name together
+    // If user has entered both country name and city name together.
     const nameSplit = name.toLowerCase().split(' ')
+    // User entered text matched with the country name.
+    const matchedCountryName = lowerCountryName === userEnteredCountryName || nameSplit.includes(lowerCountryName)
 
-    return lowerCountryName === userEnteredCountryName || nameSplit.includes(lowerCountryName)
+    if (matchedCountryName) {
+      return true
+    }
+
+    // Matching user entered text with cities name
+    const matchCityName = country.cities.find((cityName) => {
+      return cityName.toLowerCase() === name.toLowerCase()
+    })
+
+    if (matchCityName) {
+      return true
+    }
+
+    return false
   })
 
   if (!matchedCountry) {
@@ -41,5 +61,5 @@ function getTimezonesWithCountryCode(code) {
 
 module.exports = {
   getTimezonesWithCountryCode: getTimezonesWithCountryCode,
-  getTimezonesWithCountryName: getTimezonesWithCountryName
+  getTimezones: getTimezones
 }
